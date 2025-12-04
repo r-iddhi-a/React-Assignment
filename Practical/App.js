8168e9de1c6bd58d24afe21e23801aa92f90380c
r-iddhi-a <riddhigodham9@gmail.com>
2025-12-04 18:00:51 +0530

    Add files via upload

---
+++ Practical/App.js
@@ -0,0 +1,38 @@
+import logo from './logo.svg';
+import './App.css';
+import Hello from './Hello';
+import JsxComponent from './JsxComponent';
+import FunctionComponent from './FunctionComponent';
+import ClassComponent from './ClassComponent';
+import UserCard from './UserCard';
+import Counter from './Counter';
+import FormComponent  from './FormComponent';
+import LifeCycle from './LifeCycle';
+import Event from './Event';
+import LoginLogout from './LoginLogout';
+import VoteComponent from './VoteComponent';
+import List from './List';
+import Key from './Key';
+
+function App() {
+  return (
+     <div>
+      {/* <Hello />
+      <JsxComponent/>
+      <FunctionComponent name="Riddhi"/>
+      <ClassComponent/>
+      <UserCard name="Siya" age="21" location="India"/>
+      <Counter/> */}
+      {/* <FormComponent/> */}
+      {/* <LifeCycle/> */}
+      <Event/>
+      {/* <LoginLogout/> */}
+      {/* <VoteComponent/> */}
+      {/* <List/> */}
+      {/* <Key/> */}
+    </div>
+    
+  );
+}
+
+export default App;
